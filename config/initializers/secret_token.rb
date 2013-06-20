262c69dc0a2860567951cd2ca2b1e22481ae583e
Alan Hampson <ahampson@thoughtworks.com>
2013-06-20 02:03:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubsTranslate::Application.config.secret_token = '5d7eb1fe969379fd2dba21ca904e3e70e5c9542b668908b93be59f6a50657ab89680110f4f31f2cee50b7d9670c80a278a24a6aeb1e1ba676f53ad77c50756d1'
